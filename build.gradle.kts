a450b58b15f83b5568681036c6920d0356986fbc
Mark Baker-Munton <markbm@mongodb.com>
2023-07-04 16:13:06 +0100

    FEAT(maven publish)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     kotlin("jvm") version "1.8.22"
     alias(libs.plugins.kotlin.serialization)
+    `maven-publish`
+    `java-library`
+    signing
 }
 
 group = "uk.dioxic.kotlinx"
@@ -18,4 +21,58 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+//                name.set("My Library")
+                description.set("Kotlin Serialization for BSON")
+                url.set("https://github.com/dioxic/kotlinx-serialization-bson")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("dioxic")
+                        name.set("Mark Baker-Munton")
+                        email.set("dioxic@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/dioxic/kotlinx-serialization-bson.git")
+                    developerConnection.set("scm:git:git@github.com:dioxic/kotlinx-serialization-bson.git")
+                    url.set("https://github.com/dioxic/kotlinx-serialization-bson")
+                }
+            }
+        }
+    }
+}
+
+tasks.javadoc {
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
 }
