70bbdab6796d2004958412a8a3bf7feab08dfeab
Mark Baker-Munton <markbm@mongodb.com>
2023-07-05 23:05:26 +0100

    FIX: incorrect reference to Json

--- src/main/kotlin/kotlinx/serialization/bson/internal/UnknownKeyException.kt
+++ src/main/kotlin/kotlinx/serialization/bson/internal/UnknownKeyException.kt
@@ -1,8 +1,10 @@
+@file:Suppress("FunctionName")
+
 package kotlinx.serialization.bson.internal
 
 import kotlinx.serialization.SerializationException
 
-internal const val ignoreUnknownKeysHint = "Use 'ignoreUnknownKeys = true' in 'Json {}' builder to ignore unknown keys."
+internal const val ignoreUnknownKeysHint = "Use 'ignoreUnknownKeys = true' in 'Bson {}' builder to ignore unknown keys."
 
 internal fun UnknownKeyException(key: String) = SerializationException(
     "Encountered an unknown key '$key'.\n" +
